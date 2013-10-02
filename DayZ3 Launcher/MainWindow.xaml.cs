030de6317afefd0c4a36940f008ea0c5b7fd3dca
Chris Timberlake <game64@gmail.com>
2013-10-02 05:49:49 -0700

    Basic Setup
    This is the basic layout with the databindings for server listings
    setup. More coming soon.

---
+++ DayZ3 Launcher/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DayZ3_Launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.HideAll();
+            RecentNews.Visibility = System.Windows.Visibility.Visible;
+
+            //serverListItems list = new serverListItems();
+            //serverListItem item1 = new serverListItem(true, "1", "2", "3", "4", "5");
+            //list.Add(item1);
+        }
+
+        /*
+         * function HideAll
+         * This is used to hide all UI Elements. Rather than hide everything in the button functions.
+         */
+        private void HideAll()
+        {
+            RecentNews.Visibility = System.Windows.Visibility.Hidden;
+            ServerList.Visibility = System.Windows.Visibility.Hidden;
+            Installer.Visibility = System.Windows.Visibility.Hidden;
+            Settings.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        /*
+         * function RecentNews_Btn_MouseDown
+         * This is used to activate the recent news slide.
+         */
+        private void RecentNews_Btn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.HideAll();
+            RecentNews.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        /*
+         * function ServerList_Btn_MouseDown
+         * This is used to activate the server list slide.
+         */
+        private void ServerList_Btn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.HideAll();
+            ServerList.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        /*
+         * function Installer_Btn_MouseDown
+         * This is used to activate the installer slide.
+         */
+        private void Installer_Btn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.HideAll();
+            Installer.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        /*
+         * function Settings_Btn_MouseDown
+         * This is used to activate the settings slide.
+         */
+        private void Settings_Btn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.HideAll();
+            Settings.Visibility = System.Windows.Visibility.Visible;
+        }
+    }
+}
---
+++ DayZ3 Launcher/serverListItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DayZ3_Launcher
+{
+    class serverListItem
+    {
+        
+        public string ServerName { get; set; }
+        public string Version { get; set; }
+        public string Players { get; set; }
+        public string Ping { get; set; }
+        public string GameType { get; set; }
+
+        public CheckBox Favorite { get; set; }
+
+        public serverListItem()
+        {
+
+        }
+    }
+
+    class serverList : ObservableCollection<serverListItem>
+    {
+        ObservableCollection<serverListItem> _serverListItems = new ObservableCollection<serverListItem>();
+
+        public ObservableCollection<serverListItem> serverListItems
+        {
+            get { return this._serverListItems; }
+            set { this._serverListItems = value; }
+        }
+
+        public serverList()
+            : base()
+        {
+            serverListItems.Add(new serverListItem { Favorite = new CheckBox(), ServerName = "1", Version = "2", Players = "3", Ping = "4", GameType = "5" });
+        }
+    }
+
+}
